211675986b13f6667ce17de3f58b0aca73b07fef
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-10-23 11:49:40 +0200

    move CorpusReader into Glueckskes module

--- lib/glueckskeks/corpus_reader.rb
+++ lib/glueckskeks/corpus_reader.rb
@@ -1,18 +1,20 @@
-class CorpusReader
-  def self.read_corpus_files
-    result_map = {}
+module Glueckskeks
+  class CorpusReader
+    def self.read_corpus_files
+      result_map = {}
 
-    Dir.glob('corpus/*.corpus').each do |filename|
-      file = File.new(filename)
+      Dir.glob('corpus/*.corpus').each do |filename|
+        file = File.new(filename)
 
-      lines = file.readlines
+        lines = file.readlines
 
-      lines.map!(&:strip!)
-      lines.reject!(&:empty?)
+        lines.map!(&:strip!)
+        lines.reject!(&:empty?)
 
-      result_map[File.basename(file, '.corpus')] = lines
+        result_map[File.basename(file, '.corpus')] = lines
+      end
+
+      result_map
     end
-
-    result_map
   end
 end
